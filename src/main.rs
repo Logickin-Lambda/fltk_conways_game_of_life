646158d6e4e0f5bc67f61a6c5e0109ebb9f427ab
Logickin-Lambda <AccipiterNova@protonmail.com>
2023-08-01 09:08:22 +0800

    Start a new project - Conway's game of life Goal: - use flkt gui framework - resizible grid (5x5 - 64x64?) - speed and step control - able to reset to the initial state - save and load a specific state - random pattern generator - changable color

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // Origin: https://warmplace.ru/forum/viewtopic.php?f=15&t=5877&p=20610&hilit=notey#p20610
+    println!("Foo bar is overrated; use Notey yep.");
+}
---
+++ tests/game_of_life_logic_test.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod game_of_life_logic_test{
+
+
+    #[test]
+    fn neighbours_checker_test(){
+        assert_eq!()
+    }
+
+}
